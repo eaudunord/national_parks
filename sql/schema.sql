72c5f36cacadc36fbffc65632687e6974ba161d8
Joe Sellner <joe@crwd.local>
2021-06-11 21:29:54 -0500

    trails_transformed

--- sql/schema.sql
+++ sql/schema.sql
@@ -27,6 +27,50 @@ CREATE TABLE "national_parks" (
     "latitude" decimal   NOT NULL,
     "longitude" decimal   NOT NULL,
     CONSTRAINT "pk_national_parks" PRIMARY KEY (
-        "id"
+        "park_name"
      )
+);
+
+CREATE TABLE "park_trails" (
+	"trail_id" int PRIMARY KEY,
+	"trail_name" varchar,
+	"park_name" varchar(100),
+	"city" varchar,
+	"state" varchar,
+	"country" varchar,
+	"popularity" float,
+	"length" float,
+	"elevation_gain" float,
+	"difficulty rating" int,
+	"route_type" varchar,
+	"visitor_usage" decimal,
+	"avg_rating" decimal,
+	"num_reviews" int,
+	"units" char(1),
+	"latitude" decimal,
+	"longitude" decimal,
+	CONSTRAINT "fk_park_name" FOREIGN KEY("park_name")
+	REFERENCES national_parks(park_name)
+);
+
+CREATE TABLE "trail_activities" (
+	id int PRIMARY KEY,
+	"trail_id" int,
+	"park_name" varchar(100),
+	"activities" varchar,
+	CONSTRAINT "fk_park_name" FOREIGN KEY("park_name")
+	REFERENCES national_parks(park_name),
+	CONSTRAINT "fk_trail_id" FOREIGN KEY("trail_id")
+	REFERENCES park_trails(trail_id)
+);
+
+CREATE TABLE "trail_features" (
+	id int PRIMARY KEY,
+	"trail_id" int,
+	"park_name" varchar(100),
+	"features" varchar,
+	CONSTRAINT "fk_park_name" FOREIGN KEY("park_name")
+	REFERENCES national_parks(park_name),
+	CONSTRAINT "fk_trail_id" FOREIGN KEY("trail_id")
+	REFERENCES park_trails(trail_id)
 );
